fbf72d960506d721d5d18e69e2b88c8912eb0341
Rodrigom96 <rodrigom1996@gmail.com>
2021-12-17 09:44:27 -0300

    add rtsp sink

--- src/pipeline/sinks/mod.rs
+++ src/pipeline/sinks/mod.rs
@@ -7,6 +7,7 @@ use common::MissingElement;
 
 mod msg_broker;
 mod render_sink;
+mod rtsp_sink;
 
 pub fn create_sink_bin(config: SinksConfig) -> Result<gst::Bin, Error> {
     let bin = gst::Bin::new(Some("sink_bin"));
@@ -16,32 +17,36 @@ pub fn create_sink_bin(config: SinksConfig) -> Result<gst::Bin, Error> {
     bin.add_many(&[&queue, &tee])?;
     queue.link(&tee)?;
 
-    if config.display {
-        // Add filter to proccess images for display
-        let display_queue = gst::ElementFactory::make("queue", None)
-        .map_err(|_| MissingElement("queue"))?;
-        let nvvidconv = gst::ElementFactory::make("nvvideoconvert", None)
-            .map_err(|_| MissingElement("nvvideoconvert"))?;
-        let nvosd =
-            gst::ElementFactory::make("nvdsosd", None).map_err(|_| MissingElement("nvdsosd"))?;
-        let tiler = gst::ElementFactory::make("nvmultistreamtiler", None)
-            .map_err(|_| MissingElement("nvmultistreamtiler"))?;
-        let tee_display =
-            gst::ElementFactory::make("tee", None).map_err(|_| MissingElement("tee"))?;
-        bin.add_many(&[&display_queue, &nvvidconv, &nvosd, &tiler, &tee_display])?;
-        common::link_element_to_tee_src_pad(&tee, &display_queue)?;
-        display_queue.link(&nvvidconv)?;
-        nvvidconv.link(&nvosd)?;
-        nvosd.link(&tiler)?;
-        tiler.link(&tee_display)?;
+    // Add filter to proccess images for display
+    let display_queue =
+        gst::ElementFactory::make("queue", None).map_err(|_| MissingElement("queue"))?;
+    let nvvidconv = gst::ElementFactory::make("nvvideoconvert", None)
+        .map_err(|_| MissingElement("nvvideoconvert"))?;
+    let nvosd =
+        gst::ElementFactory::make("nvdsosd", None).map_err(|_| MissingElement("nvdsosd"))?;
+    let tiler = gst::ElementFactory::make("nvmultistreamtiler", None)
+        .map_err(|_| MissingElement("nvmultistreamtiler"))?;
+    let tee_display = gst::ElementFactory::make("tee", None).map_err(|_| MissingElement("tee"))?;
+    bin.add_many(&[&display_queue, &nvvidconv, &nvosd, &tiler, &tee_display])?;
+    common::link_element_to_tee_src_pad(&tee, &display_queue)?;
+    display_queue.link(&nvvidconv)?;
+    nvvidconv.link(&nvosd)?;
+    nvosd.link(&tiler)?;
+    tiler.link(&tee_display)?;
 
-        // Add sinks
+    // Add rtsp sink
+    let rtsp_sink = rtsp_sink::create_bin(Some("rtsp_sink"))?;
+    bin.add(&rtsp_sink)?;
+    common::link_element_to_tee_src_pad(&tee_display, &rtsp_sink)?;
+
+    // Add display sinks
+    if config.display {
         let render_sink = render_sink::create_bin(Some("render_sink"))?;
         bin.add(&render_sink)?;
         common::link_element_to_tee_src_pad(&tee_display, &render_sink)?;
     }
 
-    // add kafka msg broker
+    // Add kafka msg broker
     let broker = msg_broker::create_bin(Some("kafkamsgbroker_sink"), config.msg_broker)?;
     bin.add(&broker)?;
     common::link_element_to_tee_src_pad(&tee, &broker)?;
