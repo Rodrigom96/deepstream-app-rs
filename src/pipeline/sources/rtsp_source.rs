cd9cd47b9ef58eb32c1bcd23a38851fcf78d1179
Rodrigo Moreira <rodrigom1996@gmail.com>
2022-02-27 16:09:13 -0300

    rtspsrc, select only video stream

--- src/pipeline/sources/rtsp_source.rs
+++ src/pipeline/sources/rtsp_source.rs
@@ -1,11 +1,39 @@
 use anyhow::Error;
 use gst::prelude::*;
-use log::{debug, error, info, warn};
+use log::{debug, error, warn};
 
 use super::super::common;
 use super::super::common::MissingElement;
 
 use super::base_source::Source;
+
+fn pad_add_handler(src: &gst::Element, src_pad: &gst::Pad, sink: &gst::Element) {
+    debug!("Received new pad {} from {}", src_pad.name(), src.name());
+
+    let sink_pad = sink
+        .static_pad("sink")
+        .expect("Failed to get static sink pad from convert");
+    if sink_pad.is_linked() {
+        warn!("{} already linked. Ignoring.", sink.name());
+        return;
+    }
+    let new_pad_caps = match src_pad.current_caps() {
+        Some(cap) => cap,
+        None => src_pad.query_caps(None),
+    };
+    let new_pad_struct = new_pad_caps
+        .structure(0)
+        .expect("Failed to get first structure of caps.");
+    let new_pad_type = new_pad_struct.name();
+    debug!("Received pad type {} from {}", new_pad_type, src.name());
+
+    let res = src_pad.link(&sink_pad);
+    if res.is_err() {
+        error!("Type is {} but link failed.", new_pad_type);
+    } else {
+        debug!("Link succeeded (type {}).", new_pad_type);
+    }
+}
 
 pub struct RTSPSource {
     bin: gst::Bin,
@@ -33,77 +61,31 @@ impl RTSPSource {
         // Add bin sink ghostpad
         common::add_bin_ghost_pad(&bin, &queue, "src")?;
 
+        // Only select video stream
+        rtspsrc.connect("select-stream", false, |args| {
+            let caps = args[2].get::<gst::Caps>().unwrap();
+            let caps_struct = caps.structure(0).expect("Failed to get structure of caps.");
+            let is_video = caps_struct.to_string().contains("media=(string)video");
+            Some(is_video.to_value())
+        })?;
+
         // Connect the pad-added signal
         let decodebin_weak = decodebin.downgrade();
         rtspsrc.connect_pad_added(move |src, src_pad| {
-            debug!("Received new pad {} from {}", src_pad.name(), src.name());
             let decodebin = match decodebin_weak.upgrade() {
                 Some(decodebin) => decodebin,
                 None => return,
             };
-
-            let sink_pad = decodebin
-                .static_pad("sink")
-                .expect("Failed to get static sink pad from convert");
-            if sink_pad.is_linked() {
-                warn!("We are already linked. Ignoring.");
-                return;
-            }
-            let new_pad_caps = match src_pad.current_caps() {
-                Some(cap) => cap,
-                None => src_pad.query_caps(None),
-            };
-            let new_pad_struct = new_pad_caps
-                .structure(0)
-                .expect("Failed to get first structure of caps.");
-            let new_pad_type = new_pad_struct.name();
-            debug!("Received pad type {}", new_pad_type);
-
-            let res = src_pad.link(&sink_pad);
-            if res.is_err() {
-                error!("Type is {} but link failed.", new_pad_type);
-            } else {
-                info!("Link succeeded (type {}).", new_pad_type);
-            }
+            pad_add_handler(src, src_pad, &decodebin);
         });
 
         let queue_weak = queue.downgrade();
         decodebin.connect_pad_added(move |src, src_pad| {
-            debug!("Received new pad {} from {}", src_pad.name(), src.name());
             let queue = match queue_weak.upgrade() {
                 Some(queue) => queue,
                 None => return,
             };
-
-            let sink_pad = queue
-                .static_pad("sink")
-                .expect("Failed to get static sink pad from convert");
-            if sink_pad.is_linked() {
-                warn!("We are already linked. Ignoring.");
-                return;
-            }
-            let new_pad_caps = match src_pad.current_caps() {
-                Some(cap) => cap,
-                None => src_pad.query_caps(None),
-            };
-            let new_pad_struct = new_pad_caps
-                .structure(0)
-                .expect("Failed to get first structure of caps.");
-            let new_pad_type = new_pad_struct.name();
-            debug!("Received pad type {}", new_pad_type);
-            
-            let is_video = new_pad_type.starts_with("video/");
-            if !is_video {
-                debug!("It has type {} which is not video. Ignoring.", new_pad_type);
-                return;
-            }
-
-            let res = src_pad.link(&sink_pad);
-            if res.is_err() {
-                error!("Type is {} but link failed.", new_pad_type);
-            } else {
-                info!("Link succeeded (type {}).", new_pad_type);
-            }
+            pad_add_handler(src, src_pad, &queue);
         });
 
         Ok(RTSPSource { bin })
